4cb286873c30385f43332205e61817733233faf4
Simon <168045868+simon-javier@users.noreply.github.com>
2025-07-09 06:50:40 +0800

    questions form progress

--- pages/urls.py
+++ pages/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from .views import HomePageView
+from .views import HomePageView, QuestionsFormView
 
 urlpatterns = [
     path("", HomePageView.as_view(), name="home"),
+    path("questions-form/", QuestionsFormView.as_view(), name="questions-form"),
 ]
--- pages/views.py
+++ pages/views.py
@@ -1,5 +1,12 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import TemplateView
 
 
 class HomePageView(TemplateView):
     template_name = "home.html"
+
+
+class QuestionsFormView(LoginRequiredMixin, TemplateView):
+    template_name = "questions-form.html"
+    login_url = "home"
+    redirect_field_name = "redirect_to"
